ddeface88d8105078f3c8b43fbbaba8805dacee0
Hantsaniala Eléo <hantsaniala@gmail.com>
2024-09-28 16:09:59 +0300

    feat(parser): add `ProcessFile`

---
+++ pkg/parser/parser.go
@@ -0,0 +1,36 @@
+package parser
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"path/filepath"
+
+	"github.com/gomaki/gmk/pkg/generator"
+)
+
+func ProcessFile(filenamePath string) {
+	fset := token.NewFileSet()
+
+	// Parse the Go source file
+	file, err := parser.ParseFile(fset, filenamePath, nil, parser.AllErrors)
+	if err != nil {
+		fmt.Println("Error parsing file:", err)
+		return
+	}
+	packageName := file.Name.Name
+	dest := filepath.Dir(filenamePath)
+
+	// Traverse the AST and find structs
+	ast.Inspect(file, func(n ast.Node) bool {
+		if ts, ok := n.(*ast.TypeSpec); ok {
+			if structType, ok := ts.Type.(*ast.StructType); ok {
+				structName := ts.Name.Name
+				fmt.Printf("Found struct: %s\n", structName)
+				generator.GenerateMethods(packageName, structName, structType, dest)
+			}
+		}
+		return true
+	})
+}
