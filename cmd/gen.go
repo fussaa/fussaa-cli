b04750be5c4e8909b6d344613c860db1f290beb5
Hantsaniala Eléo <hantsaniala@gmail.com>
2024-09-28 16:11:07 +0300

    feat(cmd): add `gen` command

---
+++ cmd/gen.go
@@ -0,0 +1,27 @@
+// cmd/gen.go
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/gomaki/gmk/pkg/parser"
+	"github.com/spf13/cobra"
+)
+
+var genCmd = &cobra.Command{
+	Use:   "gen [file]",
+	Short: "Generate methods from struct existing struct(s)",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 1 {
+			fmt.Println("Please provide the Go file path.")
+			return
+		}
+		filePath := args[0]
+		parser.ProcessFile(filePath)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(genCmd)
+}
